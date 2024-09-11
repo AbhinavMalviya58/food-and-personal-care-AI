12e5ab2ef97cd8d9ea7dbab0526a85b888cc6849
sahilq312 <quraishisahil04@outlook.com>
2024-09-11 16:45:08 +0530

    commit #1
    added a form to send image in base64 with question in test  to backend api . where we can send data to  gemine AI
    for prompting

---
+++ web/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+/* 
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input"; */
+//import Image from "next/image";
+import ImageBase from "@/components/imageBase";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <ImageBase/>
+    </div>
+  );
+}
