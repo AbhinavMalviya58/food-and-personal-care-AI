dddac93fd2456d5b1841ae639948e6afc359fc40
dev-rathore <rathoredvd3001@gmail.com>
2024-09-12 01:16:32 +0530

    organized files

--- web/app/api/prompt/text/route.ts
+++ web/app/api/prompt/text/route.ts
@@ -1,48 +1,11 @@
-import { generationConfig, model, uploadToGemini } from "@/lib/gemini/config";
+import { promptAsText } from "@/lib/gemini/prompt-text";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    const formData = await request.json();
+    const body = await request.json();
 
-    // if (!formData.has("image" || !formData.has("prompt"))) {
-    //   return NextResponse.json(
-    //     { error: "Missing required parameters" },
-    //     { status: 400 }
-    //   );
-    // }
-
-    // let files: any = [];
-    // let fileData: any = {};
-
-    // // @ts-ignore
-    // if (formData.image) {
-    //   // @ts-ignore
-    //   files = [await uploadToGemini(formData.image, "image/jpeg")];
-
-    //   fileData = {
-    //     mimeType: files[0].mimeType,
-    //     fileUri: files[0].uri,
-    //   };
-    // }
-
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-        // {
-        // role: "user",
-        // parts: [
-        //   {
-        //     // fileData,
-        //   },
-        // ],
-        // },
-      ],
-    });
-    console.log("Prompt", formData);
-
-    // @ts-ignore
-    const result = await chatSession.sendMessage(formData.prompt);
+    const result = await promptAsText(body.prompt);
 
     return NextResponse.json({
       result: result.response.text(),
