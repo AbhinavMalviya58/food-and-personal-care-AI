73a1b94b54049e45b1ac4c809d759e674ea68f82
sahilq312 <quraishisahil04@outlook.com>
2024-09-14 22:46:58 +0530

    Jai mata di code maat faat na plz

---
+++ web/src/components/feature-card-inputs/index.ts
@@ -0,0 +1,6 @@
+import FeatureCard1 from "./feature-card-1";
+import FeatureCard2 from "./feature-card-2";
+import FeatureCard3 from "./feature-card-3";
+import FeatureCard7 from "./feature-card-7";
+
+export { FeatureCard1, FeatureCard2, FeatureCard3, FeatureCard7 };
