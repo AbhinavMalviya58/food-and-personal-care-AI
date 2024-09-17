8579ee78ce76ff2aa049cc999d9d1d1526d6a69c
Devender Singh Rathore <59788555+dev-rathore@users.noreply.github.com>
2024-09-17 20:18:52 +0530

    App flow structure (#17)
    * app flow structure layout

--- web/src/app/(dashboard)/dashboard/page.tsx
+++ web/src/app/(dashboard)/dashboard/page.tsx
@@ -1,68 +1,64 @@
 'use client';
 
-import React, { useState } from "react";
+import React from "react";
 import { FaRunning } from "react-icons/fa";
-import InputRender from "./input-render";
 import { ModeToggle } from "@/components/toogle";
-
+import { useRouter } from "next/navigation";
+import ROUTES from "@/lib/constants/routes";
 
 const FEATURE_CARDS = [
   {
+    description: "Food AI",
     id: '1',
-    description: "I want to find out ingredients in my food are healthy or not",
+    route: ROUTES.FOOD_AI,
   },
   {
+    description: "Personal Care AI",
     id: '2',
-    description: "I am suffering from a health condition and want to know what I should eat",
+    route: ROUTES.PERSONAL_CARE_AI,
   },
   {
+    description: "Chats",
     id: '3',
-    description: "Create a personalized diet plan tailored to my needs",
+    route: ROUTES.CHATS,
   },
   {
+    description: "User Settings",
     id: '4',
-    description: "How much protein, carbs, and fats are in my food?",
-  },
-  {
-    id: '5',
-    description: "I want to learn how I can cook healthier meals",
-  },
-  {
-    id: '6',
-    description: "I want to know about a particular food item",
-  },
-  {
-    id: '7',
-    description: "Give me a random food fact",
+    route: ROUTES.USER_SETTINGS,
   },
 ];
 
 type FeatureCard = {
-  id: string;
   description: string;
+  id: string;
+  route: string;
 }
 
 const Dashboard = () => {
-  const [selectedCard, setSelectedCard] = useState<FeatureCard>();
+  const router = useRouter();
 
   const handleCardSelection = (card: FeatureCard) => {
-    setSelectedCard(card);
+    router.push(`${card.route}`);
   }
 
   return (
     <div className="flex p-8 gap-4 h-screen bg-app-dark">
       <div className="flex flex-col items-center w-2/3 gap-36 mx-auto">
-        <div className="flex flex-1 flex-col items-center gap-16">
+        <div className="w-full flex flex-1 flex-col items-center gap-16">
           <h1 className="flex gap-3 text-star-white cursor-pointer text-5xl font-secondary">
-            Better <span className="text-app-primary flex gap-2 items-center">Health <FaRunning size={48} /></span>
-          <ModeToggle/></h1>
-          
-          <div className="flex flex-wrap gap-6 items-center justify-center">
+            Better <span className="text-app-primary flex gap-2 items-center">
+              Health <FaRunning size={48} />
+            </span>
+            <ModeToggle />
+          </h1>
+
+          <div className="w-2/3 grid grid-cols-2 gap-6 items-center justify-center">
             {
               FEATURE_CARDS.map((card) => (
                 <div
                   key={card.id}
-                  className={`flex max-w-96 min-h-24 items-center gap-3 text-star-white cursor-pointer p-5 rounded-lg border-4 ${selectedCard?.id === card.id ? 'border-app-primary' : 'border-gray-1 hover:border-gray-2'}`}
+                  className={`flex min-h-24 items-center justify-center text-star-white cursor-pointer p-5 rounded-lg border-4 border-app-primary`}
                   onClick={() => handleCardSelection(card)}
                 >
                   <p className="text-center text-base">{card.description}</p>
@@ -71,16 +67,6 @@ const Dashboard = () => {
             }
           </div>
         </div>
-        <div className="w-full flex justify-end items-end gap-4 min-h-64 p-8">
-          {
-            selectedCard && (
-              <InputRender id={selectedCard.id} />
-            )
-          }
-        </div>
-        {/* <div className="flex flex-col items-center justify-center w-24 h-12 bg-[#28b463] hover:shadow-sm hover:bg-[#28b463]/90 cursor-pointer rounded-md text-[#18392A] text-base uppercase font-bold">
-          Submit
-        </div> */}
       </div>
     </div>
   )
