9cfda336036b356c769504751b71406a17b372c5
sahilq312 <quraishisahil04@outlook.com>
2024-09-22 23:35:17 +0530

    Update route.ts

--- web/src/app/api/test/route.ts
+++ web/src/app/api/test/route.ts
@@ -21,47 +21,23 @@ export async function POST(req: Request) {
     // prompt
     try {
         const { prompt } = await req.json();
-        const imageUrl = ["https://utfs.io/f/8d8DWWhM1OVp2kumzdiU1gzqky8sLW2cXlmnGNAFH0jJ5dth","https://utfs.io/f/8d8DWWhM1OVpHfRbzpkANuvKlFgSzpIMj7odOn5EQRrk9L4V"];
+        const imageUrl = ["https://utfs.io/f/8d8DWWhM1OVp2kumzdiU1gzqky8sLW2cXlmnGNAFH0jJ5dth"];
         // Fetch the image data
-        const imageResponse = await fetch(imageUrl[1]);
+        const imageResponse = await fetch(imageUrl[0]);
         if (!imageResponse.ok) {
             throw new Error("Failed to fetch image");
         }
         const imageBuffer = await imageResponse.arrayBuffer();
         const imageBase64 = Buffer.from(imageBuffer).toString("base64");
+        //console.log(imageBase64);
+        
 
         const chat = model.startChat({
              history : [
                 {
                     role: "user",
-                    parts: [{ text: "hii" }],
-                },
-                {
-                    role: "model",
-                    parts: [{ text: "Hello! 👋 \n" }],
-                },
-                {
-                    role: "user",
                     parts: [
-                        { text: `what is this image: ${imageUrl[0]}` },
-                        {
-                            inlineData: {
-                                mimeType: imageResponse.headers.get("content-type") || "image/jpeg",
-                                data: imageBase64
-                            }
-                        }
-                    ]
-                },
-                {
-                    role : "model",
-                    parts : [
-                        {text : "The image shows a list of ingredients for a food product. It contains a lot of information about the food, including:\n\n* **The primary ingredients:** Gram pulse flour, edible vegetable oil (cotton seed, corn, palmolein, ground nut and rice bran), gram pulse, lentil, peanuts, rice flakes, potatoes, tepary beans flour, iodised salt, black salt powder, spinach powder, rice powder, edible starch.\n* **A list of spices and condiments:** Red chili powder, turmeric powder, mango powder, black pepper powder, ginger powder, clove powder, mace powder, nutmeg powder, cumin powder, fenugreek seeds powder, coriander powder, bay leaves powder, mint leaves powder, cardamom powder, asafoetida powder\n* **Additives:** Acidity regulator (INS 330), anticaking agent (INS 551), and synthetic food colour (INS 133).\n* **Allergen Information:** The product contains peanuts.\n\nIt appears to be a list of ingredients from a package of some type of Indian snack food.  \n\nLet me know if you have any questions about these ingredients or if you want to know more about this product! \n"}
-                    ]
-                },
-                {
-                    role: "user",
-                    parts: [
-                        { text: `what is this image: ${imageUrl[1]}` },
+                        /* { text: `what is this image: ${imageUrl[0]}` }, */
                         {
                             inlineData: {
                                 mimeType: imageResponse.headers.get("content-type") || "image/jpeg",
